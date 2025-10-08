37b9425b477dfe150f46fccd67bbef129ff1164f
Michal <m.horodelski@medidok.de>
2025-10-08 17:54:58 +0200

    Add project files.

---
+++ KowalskiApp.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KowalskiApp.Lib
+{
+    public class Class1
+    {
+    }
+}
