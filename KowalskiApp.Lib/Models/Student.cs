aae6331bbae6555e83a4fea1a89580b2285581e7
Michal <m.horodelski@medidok.de>
2025-10-08 18:55:57 +0200

    Student added to lib

---
+++ KowalskiApp.Lib/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace KowalskiApp.Lib.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public Student(string name, string firstname)
+        {
+            Name = name;
+            FirstName = firstname;
+        }
+
+        public Student()
+        {
+            Name = "Anonim";
+            FirstName = "Gal";
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {FirstName}";
+        }
+    }
+}
